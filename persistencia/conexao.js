64d4ea0bbc555fcc4647ef879c47cbc69fb4813c
Igor Ruan <82479832+igor89ruan@users.noreply.github.com>
2024-03-01 21:45:57 -0300

    atualizacao

--- persistencia/conexao.js
+++ persistencia/conexao.js
@@ -6,5 +6,24 @@ import mysql from  'mysql2/promise';
 // reponder de imediato, demorar para responder ou numca responder
 // pos isso vamos usar o async e o await
 export default function conectar() {
-    
+    if (global.pool !== undefined) {
+        return await global.pool.getConnection();
+    } else {
+        const pool = mysql.createPool({
+            host: 'localhost',
+            user: 'root', //não é recomendado usar o super usuario
+            password: '',
+            database: 'backend',
+            waitForConnections: true,
+            connectionLimit: 10,
+            maxIdle: 10, // Máximo de conexões inativas; o valor padrão é o mesmo que "connectionLimit"
+            idleTimeout: 360000, // Tempo limite das conexões inativas em milissegundos; o valor padrão é "60000"
+            queueLimit: 0,
+            enableKeepAlive: true,
+            keepAliveInitialDelay: 0,
+        });
+
+        global.pool = pool;
+        return await pool.getConnection();
+    }
 }
