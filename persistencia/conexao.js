7bd70903dc24233054c8c364099c43b8da597b61
Igor Ruan <82479832+igor89ruan@users.noreply.github.com>
2024-03-01 21:13:16 -0300

    atualizacao

--- persistencia/conexao.js
+++ persistencia/conexao.js
@@ -1,5 +1,10 @@
-
 // O banco de dados escolhido será o MYSQL
+import mysql from  'mysql2/promise';
+
+// Nós vamos desenvolvedor código assíncrono, já que a nossa aplicação 
+// não é o banco de dados e sim se comunica com um banco de dados que pode
+// reponder de imediato, demorar para responder ou numca responder
+// pos isso vamos usar o async e o await
 export default function conectar() {
     
 }
